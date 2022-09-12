0eee807dc7983ef6bc191cd7e9bbbafef64ce7ef
ushan1996 <64035537+ushan1996@users.noreply.github.com>
2022-09-13 01:23:38 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
 
 import React ,{  useState } from "react";
 import styled, { ThemeProvider } from "styled-components";
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Table from "./components/Table.jsx";
-
-
 const Container = styled.div`
   display: flex;
 `;
-
-
-
 function App() {
-
-
   return (
-    
       <Container>
-       
           <Table />
-   
       </Container>
-  
   );
 }
 
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,58 +1,61 @@
 import React from 'react'
 import "./table.css";
-
-
 function Table() {
+    var heading = ['Name', 'Random', 'Link', 'Point'];
+    var body =
+        [['Kapil', 11, 'MCA','32'],
+        ['Aakash', 22, 'Btech','21'],
+        ['Mani', 33, 'MSc','23'],
+        ['Yash', 44, 'Mtech','43']
+        ];
     const data = [
         { name: "Anom", randomnumber: 19, link: "https://create-react-app.dev/docs/getting-started/" , point: "32" },
-        { name: "Megha", randomnumber: 19, link: "Female" , point: "32" },
-        { name: "Subham", randomnumber: 25, link: "Male" , point: "32" },
-        { name: "Subham", randomnumber: 25, link: "Male" , point: "32" },
-        { name: "Subham", randomnumber: 25, link: "Male" , point: "32" },
-        { name: "Subham", randomnumber: 25, link: "Male" , point: "32" },
-        { name: "Subham", randomnumber: 25, link: "Male" , point: "32" },
-        { name: "Subham", randomnumber: 25, link: "Male" , point: "32" },
-        { name: "Subham", randomnumber: 25, link: "Male" , point: "32" },
-        { name: "Subham", randomnumber: 25, link: "Male" , point: "32" },
-        { name: "Subham", randomnumber: 25, link: "Male" , point: "32" },
-        { name: "Subham", randomnumber: 25, link: "Male" , point: "32" },
-        { name: "Subham", randomnumber: 25, link: "Male" , point: "32" },
-        { name: "Subham", randomnumber: 25, link: "Male" , point: "32" },
-        { name: "Subham", randomnumber: 25, link: "Male" , point: "32" },
-        { name: "Subham", randomnumber: 25, link: "Male" , point: "32" },
-        { name: "Subham", randomnumber: 25, link: "Male" , point: "32" },
-        { name: "Subham", randomnumber: 25, link: "Male" , point: "32" },
-        { name: "Subham", randomnumber: 25, link: "Male" , point: "32" },
-        { name: "Subham", randomnumber: 25, link: "Male" , point: "32" },
-        { name: "Subham", randomnumber: 25, link: "Male" , point: "32" },
-        { name: "Subham", randomnumber: 25, link: "Male" , point: "32" },
+        { name: "Anom", randomnumber: 19, link: "https://create-react-app.dev/docs/getting-started/" , point: "32" },
+        { name: "Anom", randomnumber: 19, link: "https://create-react-app.dev/docs/getting-started/" , point: "32" },
+        { name: "Anom", randomnumber: 19, link: "https://create-react-app.dev/docs/getting-started/" , point: "32" },
+        { name: "Anom", randomnumber: 19, link: "https://create-react-app.dev/docs/getting-started/" , point: "32" },
+        { name: "Anom", randomnumber: 19, link: "https://create-react-app.dev/docs/getting-started/" , point: "32" },
+        { name: "Anom", randomnumber: 19, link: "https://create-react-app.dev/docs/getting-started/" , point: "32" },
+        { name: "Anom", randomnumber: 19, link: "https://create-react-app.dev/docs/getting-started/" , point: "32" },
+        { name: "Anom", randomnumber: 19, link: "https://create-react-app.dev/docs/getting-started/" , point: "32" },
+        { name: "Anom", randomnumber: 19, link: "https://create-react-app.dev/docs/getting-started/" , point: "32" },
+        { name: "Anom", randomnumber: 19, link: "https://create-react-app.dev/docs/getting-started/" , point: "32" },
+        { name: "Anom", randomnumber: 19, link: "https://create-react-app.dev/docs/getting-started/" , point: "32" },
+        { name: "Anom", randomnumber: 19, link: "https://create-react-app.dev/docs/getting-started/" , point: "32" },
+        { name: "Anom", randomnumber: 19, link: "https://create-react-app.dev/docs/getting-started/" , point: "32" },
+        { name: "Anom", randomnumber: 19, link: "https://create-react-app.dev/docs/getting-started/" , point: "32" },
+        { name: "Anom", randomnumber: 19, link: "https://create-react-app.dev/docs/getting-started/" , point: "32" },
+        { name: "Anom", randomnumber: 19, link: "https://create-react-app.dev/docs/getting-started/" , point: "32" },
+        { name: "Anom", randomnumber: 19, link: "https://create-react-app.dev/docs/getting-started/" , point: "32" },
+        { name: "Anom", randomnumber: 19, link: "https://create-react-app.dev/docs/getting-started/" , point: "32" },
+        { name: "Anom", randomnumber: 19, link: "https://create-react-app.dev/docs/getting-started/" , point: "32" },
+        { name: "Anom", randomnumber: 19, link: "https://create-react-app.dev/docs/getting-started/" , point: "32" },
+      
       ]
   return (
     <div className='root'> 
-    
-    <div className="header"><h1>Task 1</h1></div>
-    <div className='container'> 
-     <table>
-        <tr>
-          <th>Name</th>
-          <th>Random</th>
-          <th >Link</th>
-          <th className='colpoint'>Point</th>
-        </tr>
-        {data.map((val, key) => {
-          return (
-            <tr key={key}>
-              <td>{val.name}</td>
-              <td>{val.randomnumber}</td>
-              <td><a href={val.link}>{val.link}</a> </td>
-              <td className='inputpoints'><input type="text" />&nbsp;&nbsp;&nbsp;<input type="submit" /> </td>
-            </tr>
-          )
-        })}
+        <div className="header"><h1>Task 1</h1></div>
+        <div className='container'> 
+            <table>
+                <tr>   
+                    {heading.map(head => <th>{head}</th>)}
+                </tr>
+                {data.map((val, key) => {
+                return (
+                <tr key={key}>
+                    <td>{val.name}</td>
+                    <td>{val.randomnumber}</td>
+                    <td><a href={val.link}>{val.link}</a> </td>
+                    <td className='inputpoints'><input type="text" />&nbsp;&nbsp;&nbsp;<input type="submit" /> </td>
+                </tr>
+                )
+                })}
+                
       </table>
     </div>
     </div>
   )
 }
+
 
 export default Table
