f84afbe4e5a9030931e3fdb83ea208b78bda3fa6
ushan1996 <64035537+ushan1996@users.noreply.github.com>
2022-09-13 01:33:09 +0530

    update

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
+
+import React, { Component } from 'react';
 import "./table.css";
 function Table() {
     var heading = ['Name', 'Random', 'Link', 'Point'];
     var body =
-        [['Kapil', 11, 'MCA','32'],
-        ['Aakash', 22, 'Btech','21'],
-        ['Mani', 33, 'MSc','23'],
-        ['Yash', 44, 'Mtech','43']
+        [['Kapil', 11, 'https://create-react-app.dev/docs/getting-started/','32'],
+        ['Aakash', 22, 'https://create-react-app.dev/docs/getting-started/','21'],
+        ['Mani', 33, 'https://create-react-app.dev/docs/getting-started/','23'],
+        ['Yash', 44, 'https://create-react-app.dev/docs/getting-started/','43']
         ];
     const data = [
         { name: "Anom", randomnumber: 19, link: "https://create-react-app.dev/docs/getting-started/" , point: "32" },
@@ -50,12 +51,23 @@ function Table() {
                 </tr>
                 )
                 })}
-                
+                {/* <tbody>
+                    {body.map(row => <TableRow row={row} />)}
+                </tbody> */}
       </table>
     </div>
     </div>
   )
 }
 
-
+class TableRow extends Component {
+    render() {
+        var row = this.props.row;
+        return (
+            <tr>
+                {row.map(val => <td>{val}</td>)}
+            </tr>
+        )
+    }
+}
 export default Table
